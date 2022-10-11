d545dd3d321383d66608f88c823991e66f04ce09
Anh Minh <aminh09@Anhs-MacBook-Pro.local>
2022-10-11 21:00:44 +0700

    “Add”

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class MyComponent extends React.Component {
+    //JSX
+    render() {
+        return (
+            <div>
+                My First Components!
+            </div>
+        )
+    }
+}
+export default MyComponent;
