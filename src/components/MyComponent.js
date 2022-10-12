ea5c31331481c39379c014958461b4a5d1a523c9
Anh Minh <aminh09@Anhs-MacBook-Pro.local>
2022-10-12 19:34:29 +0700

    form

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,11 +4,34 @@ class MyComponent extends React.Component {
         name: 'Minh',
         age: 22
     };
+    handleClick(e) {
+        console.log("Ok", this.state.name);
+        this.setState({
+            name: 'Anh Minh',
+        })
+    }
+    handleOnchange = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+    handleOnSubmit = (e) => {
+        e.preventDefault();
+        alert("ok");
+
+    }
     //JSX
     render() {
         return (
             <div>
                 My name is {this.state.name} and age {this.state.age};
+                <form onSubmit={(e) => this.handleOnSubmit(e)}>
+                    <input
+                        type="text"
+                        onChange={(e) => this.handleOnchange(e)}
+                    />
+                    <button>Submit</button>
+                </form>
             </div>
         );
     }
